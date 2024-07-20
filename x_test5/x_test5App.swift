80bb0dbb605f166ecd0403405e3a2fcc5282c455
x_air <unchainedsoul@nate.com>
2024-07-21 07:49:18 +0900

    Initial Commit

---
+++ x_test5/x_test5App.swift
@@ -0,0 +1,17 @@
+//
+//  x_test5App.swift
+//  x_test5
+//
+//  Created by MIN GI KIM on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct x_test5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ x_test5UITests/x_test5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  x_test5UITestsLaunchTests.swift
+//  x_test5UITests
+//
+//  Created by MIN GI KIM on 7/21/24.
+//
+
+import XCTest
+
+final class x_test5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
